4d450196d4b78b52663515b2f3fc8d8742c65308
AronKronenberg <akronenberg26@sarhighschool.org>
2025-02-05 10:11:56 -0500

    Update 2/5/2025

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SAR2D2AutoLeft.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SAR2D2AutoLeft.java
@@ -16,16 +16,27 @@ public class SAR2D2AutoLeft extends LinearOpMode {
     enum State {
         DRIVE_TO_B1,
         SCORE_B1,
+        WAIT_1,
         DRIVE_TO_S2,
+        DRIVE_F_S2,
         PICK_UP_S2,
         TRANSFER_S2,
         DRIVE_TO_B2,
         SCORE_S2,
+        WAIT_2,
         DRIVE_TO_S3,
+        DRIVE_F_S3,
         PICK_UP_S3,
         TRANSFER_S3,
         DRIVE_TO_B3,
         SCORE_S3,
+        WAIT_3,
+        DRIVE_TO_S4,
+        PICK_UP_S4,
+        TRANSFER_S4,
+        DRIVE_TO_B4,
+        SCORE_S4,
+
         IDLE
     }
     State currentState = State.IDLE;
@@ -42,32 +53,51 @@ public class SAR2D2AutoLeft extends LinearOpMode {
 
         drive = new SampleMecanumDrive(hardwareMap);
 
-        Pose2d startPose = new Pose2d(0, 0, Math.toRadians(-90));
+        Pose2d startPose = new Pose2d(0, 24, Math.toRadians(-90));
         drive.setPoseEstimate(startPose);
 
         robot.setIntakePitch(0.45);
         robot.closeClaw();
 
         Trajectory drive_to_b1 = drive.trajectoryBuilder(startPose)
-                .lineToSplineHeading(new Pose2d(12,55, Math.toRadians(135)))
+                .lineToSplineHeading(new Pose2d(5,43, Math.toRadians(-45)))
                 .build();
 
         TrajectorySequence drive_to_s2 = drive.trajectorySequenceBuilder(drive_to_b1.end())
-                .splineToLinearHeading(new Pose2d(36,27, Math.toRadians(0)), Math.toRadians(0))
+                .splineToLinearHeading(new Pose2d(38.5,22, Math.toRadians(90)), Math.toRadians(0))
                 .build();
 
-        TrajectorySequence drive_to_b2 = drive.trajectorySequenceBuilder(drive_to_s2.end())
-                .splineToLinearHeading(new Pose2d(12,55, Math.toRadians(135)), Math.toRadians(0))
+        TrajectorySequence drive_f_s2 = drive.trajectorySequenceBuilder(drive_to_s2.end())
+                .forward(2)
+                .build();
+
+        TrajectorySequence drive_to_b2 = drive.trajectorySequenceBuilder(drive_f_s2.end())
+                .lineToLinearHeading(new Pose2d(5,43, Math.toRadians(-45)))
                 .build();
 
         TrajectorySequence drive_to_s3 = drive.trajectorySequenceBuilder(drive_to_b2.end())
-                .splineToLinearHeading(new Pose2d(36, 36, Math.toRadians(0)), Math.toRadians(0))
+                .splineToLinearHeading(new Pose2d(38.5, 33, Math.toRadians(90)), Math.toRadians(0))
                 .build();
 
-        TrajectorySequence drive_to_b3 = drive.trajectorySequenceBuilder(drive_to_s3.end())
-                .splineToLinearHeading(new Pose2d(12,55,Math.toRadians(135)), Math.toRadians(0))
+        TrajectorySequence drive_f_s3 = drive.trajectorySequenceBuilder(drive_to_s3.end())
+                .forward(2)
                 .build();
 
+        TrajectorySequence drive_to_b3 = drive.trajectorySequenceBuilder(drive_f_s3.end())
+                .lineToLinearHeading(new Pose2d(5,43, Math.toRadians(-45)))
+                .build();
+
+        TrajectorySequence drive_to_s4 = drive.trajectorySequenceBuilder(drive_to_b3.end())
+                .splineToLinearHeading(new Pose2d(38.5, 44, Math.toRadians(90)), Math.toRadians(0))
+                .build();
+
+        TrajectorySequence drive_f_s4 = drive.trajectorySequenceBuilder(drive_to_s4.end())
+                .forward(2)
+                .build();
+
+        TrajectorySequence drive_to_b4 = drive.trajectorySequenceBuilder(drive_f_s4.end())
+                .lineToLinearHeading(new Pose2d(5,43, Math.toRadians(-45)))
+                .build();
 
         waitForStart();
 
@@ -76,50 +106,67 @@ public class SAR2D2AutoLeft extends LinearOpMode {
         currentState = State.DRIVE_TO_B1;
         robot.intakePitch.setPosition(0.65);
         drive.followTrajectoryAsync(drive_to_b1);
-        robot.setOutakeTarget(30);
-
-        boolean manualOutake = false;
-
-
+        robot.setOutakeTarget(40);
 
         while (opModeIsActive() && !isStopRequested()) {
             switch (currentState) {
                 case DRIVE_TO_B1:
-                    if (!drive.isBusy() && !robot.isOutakeBusy()) {
+                    if (!drive.isBusy()) {
                         currentState = State.SCORE_B1;
                         robot.setBucketPos(Hardware.BucketState.BUCKET_OUT.getValue());
                         time.reset();
                     }
                     break;
                 case SCORE_B1:
-                    if (time.milliseconds() >= 1000) {
-                        currentState = State.DRIVE_TO_S2;
-                        drive.followTrajectorySequenceAsync(drive_to_s2);
+                    if (time.milliseconds() >= 1500) {
+                        currentState = State.WAIT_1;
                         robot.setBucketPos(Hardware.BucketState.BUCKET_IN.getValue());
-                        robot.setOutakeTarget(0.0);
                         time.reset();
+                    }
+                    break;
+                case WAIT_1:
+                    if (time.milliseconds() >= 500) {
+                        currentState = State.DRIVE_TO_S2;
+                        robot.setOutakeTarget(0.0);
+                        robot.setIntakePitch(0.925);
+                        drive.followTrajectorySequenceAsync(drive_to_s2);
                     }
                     break;
                 case DRIVE_TO_S2:
                     if (!drive.isBusy() && !robot.isOutakeBusy()) {
-                        currentState = State.PICK_UP_S2;
-                        robot.setIntakePitch(0);
+                        currentState = State.DRIVE_F_S2;
+                        robot.setIntakePitch(1);
                         robot.intakeWheel.setPower(INTAKE_WHEEL_SPEED);
+                        drive.followTrajectorySequenceAsync(drive_f_s2);
+
+                        time.reset();
+                    }
+                    break;
+                case DRIVE_F_S2:
+                    if (!drive.isBusy()) {
+                        currentState = State.PICK_UP_S2;
                         time.reset();
                     }
                     break;
                 case PICK_UP_S2:
-                    if (time.milliseconds() >= 750) {
+                    if (time.milliseconds() >= 1500) {
                         robot.intakeWheel.setPower(0);
                         currentState = State.TRANSFER_S2;
-                        isTransferring = true;
+
+
+                        robot.setIntakePitch(0.4);
+                        sleep(1000);
+                        robot.intakeWheel.setPower(-1);
+                        sleep(500);
+                        robot.intakeWheel.setPower(0);
+                        robot.setIntakePitch(0.55);
                     }
                     break;
                 case TRANSFER_S2:
                     if (!isTransferring) {
                         currentState = State.DRIVE_TO_B2;
                         drive.followTrajectorySequenceAsync(drive_to_b2);
-                        robot.setOutakeTarget(30);
+                        robot.setOutakeTarget(40);
                     }
                     break;
                 case DRIVE_TO_B2:
@@ -130,31 +177,53 @@ public class SAR2D2AutoLeft extends LinearOpMode {
                     }
                     break;
                 case SCORE_S2:
-                    if (time.milliseconds() >= 1000) {
-                        currentState = State.DRIVE_TO_S3;
+                    if (time.milliseconds() >= 1500) {
+                        currentState = State.WAIT_2;
+                        robot.setBucketPos(Hardware.BucketState.BUCKET_IN.getValue());
+                        time.reset();
+                    }
+                    break;
+                case WAIT_2:
+                    if (time.milliseconds() >= 500) {
                         drive.followTrajectorySequenceAsync(drive_to_s3);
                         robot.setOutakeTarget(0);
+                        currentState = State.DRIVE_TO_S3;
+                        robot.setIntakePitch(0.925);
                     }
                     break;
                 case DRIVE_TO_S3:
                     if (!drive.isBusy()) {
                         currentState = State.PICK_UP_S3;
-                        robot.setIntakePitch(0);
+                        robot.setIntakePosition(1);
                         robot.intakeWheel.setPower(INTAKE_WHEEL_SPEED);
+                        drive.followTrajectorySequenceAsync(drive_f_s3);
+                        time.reset();
+                    }
+                    break;
+                case DRIVE_F_S3:
+                    if (!drive.isBusy()) {
+                        currentState = State.PICK_UP_S3;
                         time.reset();
                     }
                     break;
                 case PICK_UP_S3:
-                    if (time.milliseconds() >= 750) {
+                    if (time.milliseconds() >= 1500) {
                         currentState = State.TRANSFER_S3;
-                        isTransferring = true;
+
+
+                        robot.setIntakePitch(0.4);
+                        sleep(1000);
+                        robot.intakeWheel.setPower(-1);
+                        sleep(500);
+                        robot.intakeWheel.setPower(0);
+                        robot.setIntakePitch(0.55);
                     }
                     break;
                 case TRANSFER_S3:
                     if (!isTransferring) {
                         currentState = State.DRIVE_TO_B3;
                         drive.followTrajectorySequenceAsync(drive_to_b3);
-                        robot.setOutakeTarget(30);
+                        robot.setOutakeTarget(40);
                     }
                     break;
                 case DRIVE_TO_B3:
@@ -166,9 +235,58 @@ public class SAR2D2AutoLeft extends LinearOpMode {
                     break;
                 case SCORE_S3:
                     if (time.milliseconds() >= 1000) {
-                        currentState = State.IDLE;
+                        robot.setBucketPos(Hardware.BucketState.BUCKET_IN.getValue());
+                        currentState = State.WAIT_3;
                     }
                     break;
+                case WAIT_3:
+                    if (time.milliseconds() >= 500) {
+                        drive.followTrajectorySequenceAsync(drive_to_s4);
+                        robot.setOutakeTarget(0);
+                        currentState = State.DRIVE_TO_S4;
+                        robot.setIntakePitch(0.925);
+                    }
+                    break;
+                case DRIVE_TO_S4:
+                    if (!drive.isBusy()) {
+                        currentState = State.PICK_UP_S4;
+                        robot.setIntakePitch(1);
+                        robot.intakeWheel.setPower(INTAKE_WHEEL_SPEED);
+                        drive.followTrajectorySequenceAsync(drive_f_s4);
+                        time.reset();
+                    }
+                    break;
+                case PICK_UP_S4:
+                    if (time.milliseconds() >= 1500) {
+                        currentState = State.TRANSFER_S4;
+
+                        robot.setIntakePitch(0.4);
+                        sleep(1000);
+                        robot.intakeWheel.setPower(-1);
+                        sleep(500);
+                        robot.intakeWheel.setPower(0);
+                        robot.setIntakePitch(0.55);
+                    }
+                    break;
+                case TRANSFER_S4:
+                    if (!isTransferring) {
+                        currentState = State.DRIVE_TO_B4;
+                        drive.followTrajectorySequenceAsync(drive_to_b4);
+                        robot.setOutakeTarget(40);
+                    }
+                    break;
+                case DRIVE_TO_B4:
+                    if (!drive.isBusy()) {
+                        currentState = State.SCORE_S4;
+                        robot.setBucketPos(Hardware.BucketState.BUCKET_OUT.getValue());
+                        time.reset();
+                    }
+                    break;
+                case SCORE_S4:
+                    if (time.milliseconds() >= 1000) {
+                        robot.setBucketPos(Hardware.BucketState.BUCKET_IN.getValue());
+                        currentState = State.IDLE;
+                    }
                 case IDLE:
                     break;
             }
